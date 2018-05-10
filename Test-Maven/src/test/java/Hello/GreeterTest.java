170733999f9bd7e5f70fb0145b4ab8ef2912c115
EricMarche <eric.marchessault@gmail.com>
2018-05-10 11:40:07 -0400

    projet test-Spring fonctionnel + debut projet test-MongoDB

---
+++ Test-Maven/src/test/java/Hello/GreeterTest.java
@@ -0,0 +1,15 @@
+package Hello;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.containsString;
+
+import org.junit.Test;
+
+public class GreeterTest {
+    private Greeter greeter = new Greeter();
+
+    @Test
+    public void greeterSaysHello() {
+        assertThat(greeter.sayHello(), containsString("Hello"));
+    }
+}
